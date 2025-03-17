ed1dbe60edbfbb3e2a539a8790790bd3d3317a9a
ansilveira <alissonns@hotmail.com>
2025-03-17 12:32:45 -0300

    Adicionando exercício

---
+++ teste.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(9, 9));
+console.log(saudacao("Alisson"));
